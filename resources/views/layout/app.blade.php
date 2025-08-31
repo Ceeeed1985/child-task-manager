18af0a6a2b0c4f4c0e5ac501cd031637dca0ed9c
Cedric Truyen <cedrictruyen@icloud.com>
2025-08-31 21:26:39 +0200

    petite modification des bordures du sidebar

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -17,10 +17,10 @@
             <span>Child Task </span>
             <span class="text-sky-700 font-bold">Manager</span>
         </a>
-
+        <!-- A voir si on ajoute => hover:drop-shadow-[0_0_5px_rgba(56,189,248,0.4)]-->
         <div class="flex flex-col justify-between flex-1 mt-12">
             <nav>
-                <a class="flex items-center mx-2 px-4 pt-2 pb-2 text-neutral-300 hover:text-white hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="{{ route('dashboard') }}">
+                <a class="flex items-center mx-2 px-4 pt-2 pb-2 text-neutral-300 hover:text-white border-b-2 border-neutral-900 hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="{{ route('dashboard') }}">
                     <svg class="size-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M19 11H5M19 11C20.1046 11 21 11.8954 21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V13C3 11.8954 3.89543 11 5 11M19 11V9C19 7.89543 18.1046 7 17 7M5 11V9C5 7.89543 5.89543 7 7 7M7 7V5C7 3.89543 7.89543 3 9 3H15C16.1046 3 17 3.89543 17 5V7M7 7H17"
@@ -29,42 +29,42 @@
                     <span class="mx-4 font-medium">Dashboard</span>
                 </a>
                 
-                <a class="flex items-center mx-2 px-4 pt-2 mt-4 pb-2 text-neutral-300 hover:text-white hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="#">
+                <a class="flex items-center mx-2 px-4 pt-2 mt-4 pb-2 text-neutral-300 hover:text-white border-b-2 border-neutral-900 hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
                     </svg>
                     <span class="mx-4 font-medium">Famille</span>
                 </a>
 
-                <a class="flex items-center mx-2 px-4 pt-2 mt-4 pb-2 text-neutral-300 hover:text-white hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="#">
+                <a class="flex items-center mx-2 px-4 pt-2 mt-4 pb-2 text-neutral-300 hover:text-white border-b-2 border-neutral-900 hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
                     </svg>
                     <span class="mx-4 font-medium">Tâches</span>
                 </a>
 
-                <a class="flex items-center mx-2 px-4 pt-2 mt-4 pb-2 text-neutral-300 hover:text-white hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="#">
+                <a class="flex items-center mx-2 px-4 pt-2 mt-4 pb-2 text-neutral-300 hover:text-white border-b-2 border-neutral-900 hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0" />
                     </svg>
                     <span class="mx-4 font-medium">Récompenses</span>
                 </a>
 
-                <a class="flex items-center mx-2 px-4 pt-2 mt-4 pb-2 text-neutral-300 hover:text-white hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="#">
+                <a class="flex items-center mx-2 px-4 pt-2 mt-4 pb-2 text-neutral-300 hover:text-white border-b-2 border-neutral-900 hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
                     </svg>
                     <span class="mx-4 font-medium">Calendrier</span>
                 </a>
 
-                <a class="flex items-center mx-2 px-4 pt-2 mt-4 pb-2 text-neutral-300 hover:text-white hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="#">
+                <a class="flex items-center mx-2 px-4 pt-2 mt-4 pb-2 text-neutral-300 hover:text-white border-b-2 border-neutral-900 hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                     </svg>
                     <span class="mx-4 font-medium">Messages</span>
                 </a>
 
-                <a class="flex items-center mx-2 px-4 pt-2 mt-4 pb-2 text-neutral-300 hover:text-white hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="#">
+                <a class="flex items-center mx-2 px-4 pt-2 mt-4 pb-2 text-neutral-300 hover:text-white border-b-2 border-neutral-900 hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5" />
                     </svg>
@@ -74,7 +74,7 @@
                 <hr class="my-6 mx-6 border-neutral-800 border-1" />
                 
                 
-                <a class="flex items-center mx-2 px-4 pt-2 mt-4 pb-2 text-neutral-300 hover:text-white hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="#">
+                <a class="flex items-center mx-2 px-4 pt-2 mt-4 pb-2 text-neutral-300 hover:text-white border-b-2 border-neutral-900 hover:border-b-2 hover:border-sky-800 focus:text-white focus:border-b-2 focus:border-sky-800" href="#">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
