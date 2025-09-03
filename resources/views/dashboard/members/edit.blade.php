099d1bfd84d144b6e0a696b7e4bfae8005bb034a
Cedric Truyen <cedrictruyen@icloud.com>
2025-09-03 11:53:52 +0200

    members.show front avancement

---
+++ resources/views/dashboard/members/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('layout.app')
+
+@section('title', "Ajouter un enfant")
+
+@section('content')
+
+<div class="pt-6 px-12">
+    <h1 class="text-sky-600 text-3xl font-bold mb-12">Ajouter un enfant</h1>
+    <div class="flex gap-8 w-1/4">
+        <form action="{{ route('members.store') }}" method="post" class="w-full" enctype="multipart/form-data">
+            @csrf
+            <div class="flex flex-col gap-2 mb-4">
+                <label for="firstname" class="text-white text-lg w-full">Prénom :</label>
+                <input id="firstname" name="firstname" type="text" class="mt-1 p-3 block w-full bg-white border border-gray-300 rounded-md shadow-md">
+            </div>
+            <div class="flex flex-col gap-2 mb-4">
+                <label for="lastname" class="text-white text-lg w-full">Nom :</label>
+                <input id="lastname" name="lastname" type="text" class="mt-1 p-3 block w-full bg-white border border-gray-300 rounded-md shadow-md">
+            </div>
+            <div class="flex flex-col gap-2 mb-6">
+                <label for="avatar" class="text-white text-lg w-full">Choisissez une photo :</label>
+                <input id="avatar" name="avatar" type="file" class="mt-1 p-3 block w-full bg-white border border-gray-300 rounded-md shadow-md">
+            </div>
+            <button type="submit" class="bg-sky-600 hover:bg-sky-800 text-white px-4 py-2 rounded">Enregistrer</button>
+        </form>
+    </div>
+    
+</div>
+
+@endsection
--- resources/views/dashboard/members/show.blade.php
+++ resources/views/dashboard/members/show.blade.php
@@ -1,18 +1,37 @@
 @extends('layout.app')
 
-@section('title', '{{ $member->firstname }}')
+@section('title', $member->firstname)
 
 @section('content')
 
 <div class="pt-6 px-12">
-    <div class="flex gap-8">
-        <div class="card w-full rounded-3xl py-6 border border-2 border-neutral-800 ">
-            <a href="#" class="flex items-center px-4 mx-2">
-                <img class="object-cover mx-2 rounded-full h-24 w-24 border border-4 border-sky-600" src="{{ asset($member->avatar) }}" alt="avatar" />
-                <span class="mx-2 font-medium text-neutral-300 text-lg pl-12">{{ $member->firstname }}</span>
-                <span class="mx-2 font-medium text-neutral-300 text-lg pl-12">{{ $member->lastname }}</span>
-                <form action="" method="post"></form>
-            </a>
+    <div class="flex gap-8 mb-12">
+        <div>
+            <img class="object-cover mx-2 rounded-full h-36 w-36 border border-6 border-sky-600" src="{{ asset($member->avatar) }}" alt="avatar" />
+        </div>
+        
+        <div class="w-full flex flex-col justify-between py-8 px-8 rounded-3xl border border-2 border-neutral-800">
+            <h3 class="text-white text-2xl">Informations du <span class="text-sky-600">profil</span></h3>
+            <div class="w-full flex justify-between rounded-3xl">
+                <div class="infos flex gap-4">
+                    <span class="mx-2 font-medium text-neutral-300 text-lg">{{ $member->firstname }}</span>
+                    <span class="mx-2 font-medium text-neutral-300 text-lg">{{ $member->lastname }}</span>
+                </div>
+                <div class="flex gap-4">
+                    <a href="" class="text-white bg-sky-600 content-center justify-self-end font-semibold px-4 rounded hover:bg-sky-800">Modifier</a>
+                    <form action="" method="post" class="text-white bg-red-700 font-semibold rounded hover:bg-red-900 px-4">
+                        @csrf
+                        @method('delete')
+                        <a href="" class="">Supprimer</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="graphique w-full h-72 flex flex-col justify-between py-8 px-8 rounded-3xl border border-2 border-neutral-800">
+        <h3 class="text-white text-2xl">Mes statistiques</h3>
+        <div class="w-full flex justify-between rounded-3xl">
         </div>
     </div>
     
